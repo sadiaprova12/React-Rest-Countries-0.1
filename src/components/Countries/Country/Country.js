7adc855513540699f489c34f4c3b2c0099d95c8f
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-13 14:53:34 +0600

    import country css file

--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Country.css'
 const Country = (props) => {
     return (
-        <div>
+        <div className='country'>
             <h2>Country Name: {props.name}</h2>
             <p>Polpulation: {props.population}</p>
             {/* <h3>Area: {props.area}</h3> */}
