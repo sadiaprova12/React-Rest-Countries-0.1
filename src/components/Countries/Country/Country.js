24dcf6aa583f29eb48c2518f398583acee8dbf37
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-13 20:38:17 +0600

    bg warning

--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -6,7 +6,7 @@ const Country = (props) => {
     const{area, region, population, name, flags} = props.country;
     console.log(props.country);
     return (
-        <div className='country'>
+        <div className='country bg-warning'>
             {/* <h2>Country Name: {props.country.name.common}</h2> */}
             <h2>Country Name: {name.common}</h2>
             <img src={flags.png} alt="" />
