56cf62f4a16920dea577ff7d3047a76d90d1698f
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-13 14:46:45 +0600

    props population and area

--- src/components/Countries/Country/Country.js
+++ src/components/Countries/Country/Country.js
@@ -4,6 +4,8 @@ const Country = (props) => {
     return (
         <div>
             <h2>Country Name: {props.name}</h2>
+            <p>Polpulation: {props.population}</p>
+            <h3>Area: {props.area}</h3>
         </div>
     );
 };
