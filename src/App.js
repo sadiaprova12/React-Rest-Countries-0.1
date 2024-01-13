d253696c6cae1ca8e000570e314a784c2f83687c
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-13 20:26:15 +0600

    anchor added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Countries from './components/Countries/Countries';
 import Footer from './components/Countries/Footer/Footer';
+import Header from './components/Countries/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
       <Footer></Footer>
     </div>
