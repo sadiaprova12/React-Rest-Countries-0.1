8b0aaa5573b0bc80103893bad8cdd0768547b3d0
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-13 20:23:19 +0600

    navbar

---
+++ src/components/Countries/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to my React Rest Countries</h2>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
